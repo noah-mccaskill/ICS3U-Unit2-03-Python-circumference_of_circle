a1cabe1a09d4a180789376b218a40e0814dfd3b4
Noah McCaskill <noah.mccaskill@mths.ca>
2022-03-28 15:17:39 +0000

    created circumference.py and constants.py

---
+++ circumference_of_circle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Noah McCaskill
+# Created on: March 2022
+# This program calculates the circumference of a circle
+
+import constants
+
+
+def main():
+    # this function calculates the circumference of a circle
+
+    # Input
+    user_radius = int(input("Enter radius:"))
+    
+    # Process
+    circumference = user_radius * constants.TAU
+
+    # Output
+    print("Circumference is {} mm².".format(circumference))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
